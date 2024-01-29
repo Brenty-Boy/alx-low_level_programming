15055b40c2af036a153d03e2859edc6afb29510c
Brenty-Boy <125645093+Brenty-Boy@users.noreply.github.com>
2024-01-30 00:59:48 +0200

    Main

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - prints the number of elements in a linked list
+ * @h: the pointer
+ * Return: the number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int number = 0;
+
+	for (; h; h = h->next)
+		number++;
+	return (number);
+}
